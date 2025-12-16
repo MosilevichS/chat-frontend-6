44b8a01bfb32518d46e894119fd6632c2939d79b
Timur <kanapin.timur@gmail.com>
2025-12-16 12:00:55 +0500

    Поменял фон контейнера auth layout на мобильных устройствах.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,8 +4,8 @@ const AuthLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="max-w-[1440px] w-full min-h-screen mx-auto bg-[linear-gradient(145.07deg,#E2EAFE_18.45%,#DCF0EE_81.96%)] md:bg-[url('/images/auth-background.png')] bg-cover bg-center">
       <div className="flex min-h-screen items-center justify-center px-4">
-        <div className="w-full max-w-[488px] h-[760px] rounded-2xl bg-[url('/images/auth-container-background.png')] bg-cover bg-center px-4 md:px-16 py-20 shadow-lg border-2 border-white md:border-none">
-          {children}
+        <div className="auth-card w-full max-w-[488px] h-[760px] rounded-2xl bg-white md:bg-[url('/images/auth-container-background.png')] bg-cover bg-center shadow-lg border-2 border-white md:border-none">
+          <div className="h-full px-4 md:px-16 py-20">{children}</div>
         </div>
       </div>
     </div>
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,11 +3,10 @@
 import Link from "next/link";
 import Logo from "@/src/components/ui/Logo";
 import Button from "@/src/components/ui/Button";
-import Tooltip from "@/src/components/ui/Tooltip";
 
 const Page = () => {
   return (
-    <div className="flex h-full flex-col items-center">
+    <div data-auth-welcome className="flex h-full flex-col items-center auth-card--mobile-bg">
       <Logo size="large" className="mb-10 md:mb-16" />
       <h2 className="text-[2.125rem] md:text-[2rem] leading-11 md:leading-8 mb-4 md:mb-6 font-extrabold md:font-semibold bg-[linear-gradient(181.47deg,#D7D7D7_-221.42%,#FFF9F9_-78.94%,#4F4C4C_36.08%)] bg-clip-text text-transparent">
         А-Чат
