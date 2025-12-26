008153658a023c9e0a4594a0d0c9578d4e75bf7f
Timur <kanapin.timur@gmail.com>
2025-12-26 17:06:20 +0500

    Поправки в BackButton и support/success page.

--- src/app/auth/support/success/page.tsx
+++ src/app/auth/support/success/page.tsx
@@ -1,58 +1,43 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Image from "next/image";
 import AuthHeader from "@/src/components/ui/auth/AuthHeader";
 import Button from "@/src/components/ui/Button";
 
-const SuccessPage = () => {
-  const router = useRouter();
-
-  const handleGoHome = () => {
-    router.push("/auth");
-  };
-
+const Page = () => {
   return (
-    <div className="flex h-full flex-col items-center pb-[1.688rem] md:pb-[5.188rem] pt-[100px] md:pt-18">
-      <div className="hidden md:block w-full max-w-[360px]">
+    <div className="flex h-full flex-col items-center pb-[1.688rem] md:pb-[5.188rem] pt-25 md:pt-18">
+      <div className="hidden md:block w-full">
         <AuthHeader className="mb-5 md:mb-8" />
       </div>
 
-      <h2 className="text-2xl md:text-[2rem] leading-[120%] md:leading-10 font-medium md:font-bold tracking-normal text-(--color-text) mb-[75px] md:mb-[30px]">
+      <h2 className="text-2xl md:text-[2rem] leading-[120%] md:leading-10 font-medium md:font-bold tracking-normal text-(--color-text) mb-18.75 md:mb-7.5">
         Служба поддержки
       </h2>
 
-      <div className="flex flex-col items-center w-full max-w-[360px] px-4 md:px-0">
-        <div className="mb-[15px] md:mb-[20px]">
-          <div className="w-[67px] h-[67px] md:w-[67px] md:h-[67px] flex items-center justify-center">
-            <img 
-              src="/assets/icons/success.svg" 
-              alt="Успешно"
-              className="w-full h-full"
-            />
-          </div>
-        </div>
+      <div className="h-full flex flex-col items-center">
+        <Image
+          src="/assets/icons/success.svg"
+          alt="Успешно"
+          width={67}
+          height={67}
+          className="mb-3.75 md:mb-5"
+        />
 
-        <h3 className="text-[24px] md:text-[24px] leading-[120%] font-medium tracking-normal text-(--color-text) text-center mb-[28px] md:mb-[24px]">
+        <h3 className="text-2xl leading-[120%] font-medium tracking-normal text-(--color-text) text-center mb-7 md:mb-6">
           Обращение отправлено!
         </h3>
 
-        <p className="text-base md:text-base leading-[140%] tracking-normal text-(--color-text) text-center max-w-[320px] md:max-w-[360px] mb-[24px] md:mb-8">
+        <p className="text-lg leading-[130%] tracking-normal text-(--color-text) text-center mb-6 md:mb-8">
           В ближайшее время вы получите ответ на электронную почту, указанную в обращении
         </p>
 
-        <div className="w-full mt-0 md:mt-[150px]">
-          <Button
-            onClick={handleGoHome}
-            variant="primary"
-            size="medium"
-            className="w-full h-14 rounded-lg"
-          >
-            На главную
-          </Button>
-        </div>
+        <Button href="/" variant="primary" size="medium" className="mt-0 md:mt-auto">
+          На главную
+        </Button>
       </div>
     </div>
   );
 };
 
-export default SuccessPage;
+export default Page;
