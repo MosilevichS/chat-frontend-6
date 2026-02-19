3e18b2621464bf7b070de0f32fda4f71803805d7
viachaslaumasilevich <mosilevich@gmail.com>
2026-02-19 12:42:56 -0800

    feature: support page added

---
+++ src/app/(main)/settings/support/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { SettingsProfilePhoto } from "@/components/ui/user-profile/profile/SettingsProfilePhoto";
+import Link from "next/link";
+import backIcon from "@/assets/icons/back-icon.svg";
+import SettingsProfileChangeForm from "@/components/ui/user-profile/profile/SettingsProfileChangeForm";
+
+const Page = () => {
+  return (
+    <div className="flex gap-x-6 w-full  justify-center">
+      <div className="w-full flex flex-col  md:max-w-[360px] min-h-[calc(100vh-84px)] mx-auto bg-(--color-gray-light) md:rounded-t-lg border border-(--color-gray-1) p-0-4-4-4">
+        <div className="w-full ">
+          <div className="flex-row items-center gap-4 flex ">
+            <Link href="/settings" className="flex p-4">
+              <Image src={backIcon} width={24} height={24} alt="" />
+            </Link>
+            <h2 className="flex justify-center text-(--color-black)  font-medium text-[1.125rem] ">
+              Обращение в поддержку
+            </h2>
+          </div>
+        dsd
+        </div>
+      </div>
+      <div className="hidden  md:flex  justify-center items-center w-full max-w-[744px] min-h-[calc(100vh-84px)] bg-(--color-gray-light) rounded-t-lg px-4"></div>
+    </div>
+  );
+};
+export default Page;
