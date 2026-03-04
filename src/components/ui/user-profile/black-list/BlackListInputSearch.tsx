c2dfbe39e9077ea58de272d7a52e69504efd0daf
viachaslaumasilevich <mosilevich@gmail.com>
2026-03-04 08:56:54 -0800

    feat: serch input added to settings black list

--- src/components/ui/user-profile/black-list/BlackListInputSearch.tsx
+++ src/components/ui/user-profile/black-list/BlackListInputSearch.tsx
@@ -1,10 +1,15 @@
-import Input from "@/components/ui/Input";
-import Vector from "@/assets/icons/Vector.svg";
+import Image from "next/image";
+import Search from "@/assets/icons/search.svg";
 export const BlackListInputSearch = () => {
   return (
-    < Input
-    placeholder= Vector "Поиск по черному списку"
-    />
+    <div className="relative">
+      <input
+        placeholder="Поиск по черному списку"
+        className="w-full pl-10 pr-4 py-2 border rounded-lg border-[var(--color-violet)] focus:outline-none"
+      />
+      <div className="absolute left-3 top-1/2 -translate-y-1/2">
+        <Image src={Search} alt="search" width={20} height={20} />
+      </div>
+    </div>
   );
 };
-
